e130e2876d9c78c58f0181c5a9eac0307ed62a08
victor <victor.psa02@gmail.com>
2021-07-01 17:50:58 -0300

    feat: language

--- script.js
+++ script.js
@@ -4,20 +4,31 @@ const scoreHG = document.querySelector(".score"),
     button = document.querySelector(".button")
 
 const words = [
-    'sigh', 'tense', 'airplane', 'ball', 'pies',
-    'juice', 'warlike', 'bad', 'north', 'dependent',
-    'steer', 'silver', 'highfalutin', 'superficial', 'quince',
-    'eight', 'feeble', 'admit', 'drag', 'loving'
+    [
+        'sigh', 'tense', 'airplane', 'ball', 'pies',
+        'juice', 'warlike', 'bad', 'north', 'dependent',
+        'steer', 'silver', 'highfalutin', 'superficial', 'quince',
+        'eight', 'feeble', 'admit', 'drag', 'loving'
+    ],
+    [
+        'suspiro', 'tenso', 'avião', 'bola', 'tortas',
+        'suco', 'guerreiro', 'mau', 'norte', 'dependente',
+        'boi', 'prata', 'pomposo', 'superficial', 'marmelo',
+        'oito', 'fraco', 'admitir', 'arrastar', 'amoroso'
+    ]
 ]
 
-let boxRandom = [],
+let boxRandom = [], randomWord,
+    lang, pt = 'pt', en = 'en',
     score = 0, turn = 0
+
+button.innerHTML = "<button onclick='setToLanguage()'>Start Game</button>"
 
 
 input.addEventListener('input', e => {
     const insertedText = e.target.value
 
-    if (insertedText === words[randomNumber]) {
+    if (insertedText === randomWord) {
         turn++
 
         wordToDOM()
@@ -29,18 +40,37 @@ input.addEventListener('input', e => {
 
 
 function startGame() {
-    wordToDOM()
-    button.style.display = "none"
     input.style.visibility = "visible"
+    wordToDOM()
     input.focus()
 }
 
-function wordToDOM() {
-    if (turn < 1) {
-        randomNumber = Math.floor(Math.random() * words.length)
-        boxRandom.push(randomNumber)
 
-        word.innerHTML = words[randomNumber]
+function setToLanguage() {
+    button.innerHTML =
+        `<h2>Choose a language </h2>
+        <button onclick='setIdiom( ${en} )'>en</button>
+        <button onclick='setIdiom( ${pt} )'>pt</button>`
+}
+
+function setIdiom(idiom) {
+    button.innerHTML = ''
+
+    if (idiom === 'en') lang = 0
+    if (idiom === 'pt') lang = 1
+
+    startGame()
+}
+
+function wordToDOM() {
+    overall = words[lang].length
+
+    if (turn < 1) {
+        randomNumber = Math.floor(Math.random() * overall)
+        randomWord = words[lang][randomNumber]
+
+        boxRandom.push(randomNumber)
+        word.innerHTML = randomWord
     } else {
         noRepeat()
     }
@@ -48,11 +78,12 @@ function wordToDOM() {
 
 function noRepeat() {
     while (boxRandom.indexOf(randomNumber) > -1) { // exists randomNumber repeats
-        randomNumber = Math.floor(Math.random() * words.length)
+        randomNumber = Math.floor(Math.random() * overall)
     }
 
+    randomWord = words[lang][randomNumber]
     boxRandom.push(randomNumber)
-    word.innerHTML = words[randomNumber]
+    word.innerHTML = randomWord
 }
 
 function updateScore() {
