d19a82e6f5da934116b00b01f05a5053d5ed3cac
victorpereiira <victorrpsantos@gmail.com>
2023-02-22 23:54:17 -0300

    update: align items and font-size

--- main.js
+++ main.js
@@ -1,5 +1,4 @@
 import App from "./src/App.js";
-import { $ } from "./src/utils/snippets.js"
 
 // const typedWordsHG = document.getElementById("typedWords")
 // const timeHG = document.getElementById("time")
@@ -14,76 +13,76 @@ import { $ } from "./src/utils/snippets.js"
     await App();
 })()
 
-async function _init_() {
-    let boxRandom = []
-    let randomWord = 0
-    let timeInterval = 0
-    let speedTime = 0
-    let lang = "english"
-    let endGame = false
-    let pause = false
-    let turn = 0
-    let time = 10
+// async function _init_() {
+//     let boxRandom = []
+//     let randomWord = 0
+//     let timeInterval = 0
+//     let speedTime = 0
+//     let lang = "english"
+//     let endGame = false
+//     let pause = false
+//     let turn = 0
+//     let time = 10
 
-    let gameTime = 0
-    let gameTimeFormat = '00:00:00'
-    let rounds = 0
-    let hitAverage = 0
-    let typedWords = 0
-    let difficultyLevel
-    let points = 0
-    let timePlayed = 0
-    let timePlayedInt = 0
-    let timePlayedFormat = 0
+//     let gameTime = 0
+//     let gameTimeFormat = '00:00:00'
+//     let rounds = 0
+//     let hitAverage = 0
+//     let typedWords = 0
+//     let difficultyLevel
+//     let points = 0
+//     let timePlayed = 0
+//     let timePlayedInt = 0
+//     let timePlayedFormat = 0
 
-    // check conditional
-    if (localStorage.getItem('speedTime') !== null) {
-        lang = Number(localStorage.getItem('lang'))
-        gameTime = Number(localStorage.getItem('gameTime'))
-        rounds = Number(localStorage.getItem('rounds'))
-        gameTimeFormat = setTimeFormat(gameTime)
+//     // check conditional
+//     if (localStorage.getItem('speedTime') !== null) {
+//         lang = Number(localStorage.getItem('lang'))
+//         gameTime = Number(localStorage.getItem('gameTime'))
+//         rounds = Number(localStorage.getItem('rounds'))
+//         gameTimeFormat = setTimeFormat(gameTime)
 
-        if (lang === 0) {
-            word.innerHTML = 'Happiness'
-            input.placeholder = 'Write the word here!!!'
-        }
+//         if (lang === 0) {
+//             word.innerHTML = 'Happiness'
+//             input.placeholder = 'Write the word here!!!'
+//         }
 
-        if (lang === 1) {
-            word.innerHTML = 'Felicidade'
-            input.placeholder = 'Escreva as palavras aqui!!!'
-        }
-    } else {
-        lang = 0
-        speedTime = 600
-    }
+//         if (lang === 1) {
+//             word.innerHTML = 'Felicidade'
+//             input.placeholder = 'Escreva as palavras aqui!!!'
+//         }
+//     } else {
+//         lang = 0
+//         speedTime = 600
+//     }
 
-    // start ui
-    drawingArea.innerHTML = draw(0)
-    input.disabled = true
+//     // start ui
+//     drawingArea.innerHTML = draw(0)
+//     input.disabled = true
 
-    // check words typing
-    input.addEventListener('input', e => {
-        const insertedText = e.target.value
+//     // check words typing
+//     input.addEventListener('input', e => {
+//         const insertedText = e.target.value
 
-        if (insertedText === randomWord) {
-            turn++
-            updateTypedWords()
-            checkLengthBoxRadom()
-            wordToDOM()
-            e.target.value = ''
+//         if (insertedText === randomWord) {
+//             turn++
+//             updateTypedWords()
+//             checkLengthBoxRadom()
+//             wordToDOM()
+//             e.target.value = ''
 
-            if (speedTime === 1000) return time += 2
-            if (speedTime === 600) return time += 3
-            if (speedTime === 300) return time += 5
-        }
-    })
+//             if (speedTime === 1000) return time += 2
+//             if (speedTime === 600) return time += 3
+//             if (speedTime === 300) return time += 5
+//         }
+//     })
 
 
-    document.addEventListener('keydown', e => {
-        if (e.key === "Enter" && !endGame) e.preventDefault()
-        else if (e.key === "Enter" && endGame) {
-            e.preventDefault()
-            resetGame()
-        }
-    });
-}
+//     document.addEventListener('keydown', e => {
+//         if (e.key === "Enter" && !endGame) e.preventDefault()
+//         else if (e.key === "Enter" && endGame) {
+//             e.preventDefault()
+//             resetGame()
+//         }
+//     });
+// }
--- src/page/Home/index.js
+++ src/page/Home/index.js
@@ -1,12 +1,35 @@
 import { $ } from "../../utils/snippets";
-// import "./_style.scss";
+import "./_style.scss";
 
 async function Home() {
-    return `
-        <div id="root">
-    
+    return /*html*/ `
+        <div id="home">
+            <header>
+                <section class="header__title">
+                    <h1>Typing Game</h1>
+                </section>
+                <section class="header__actions">
+                    <div>
+                        <p class="header__time">10<span class="header__meter-time">s<span></p>
+                        <span class="header__icon-time"></span>
+                    </div>  
+                    <button class="header__button"></button>
+                </section>
+            </header>
+            <main>
+                <h2 class="main__word">Happiness</h2>
+                <div  class="main__point">
+                    <span class="main__icon-point">.</span>
+                    <span class="main__count-point">0</span>
+                </div>
+                <input class="main__input" type="text" placeholder="Write the word here!!!">
+                <button class="main__button">Start Game</button>
+            </main>
+            <footer>
+                <p>Made by <a href="https://github.com/VictorPereiira" target="_blank">VictorPerreira</a></p>
+                <p>© 2023</p>
+            </footer>
         </div>
-    }
     `
 }
 
