1b3642cab93a3a88f8c5d33e21207963a10653fe
victorpereiira <victorrpsantos@gmail.com>
2023-02-25 19:32:30 -0300

    feat: gameover popup - structure

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import CardDefault from "./components/PopupAlpha/CardDefault/index.js";
+import CardGameOver from "./components/PopupBetha/index.js";
 import Home from "./page/Home/index.js";
 import { $ } from "./utils/snippets.js";
 
 async function App() {
-    $("body").innerHTML = await Home();
+    // $("body").innerHTML = await Home();
+    $("body").innerHTML = await CardGameOver();
     // $("body").innerHTML = await CardDefault("settings");
     // $("body").innerHTML += await CardDefault("statistics");
 }
