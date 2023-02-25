c39e0a223f6394819f47ef4c09dff75fc417aa85
victorpereiira <victorrpsantos@gmail.com>
2023-02-25 17:46:48 -0300

    feat: statistics popup - structure

--- src/App.js
+++ src/App.js
@@ -3,9 +3,9 @@ import Home from "./page/Home/index.js";
 import { $ } from "./utils/snippets.js";
 
 async function App() {
-    // $("body").innerHTML = await Home();
-    $("body").innerHTML = await CardDefault("settings");
-    // $("body").innerHTML = await CardDefault("statistics");
+    $("body").innerHTML = await Home();
+    // $("body").innerHTML = await CardDefault("settings");
+    // $("body").innerHTML += await CardDefault("statistics");
 }
 
 export default App;
--- src/components/PopupAlpha/StatisticsContent/index.js
+++ src/components/PopupAlpha/StatisticsContent/index.js
@@ -1,7 +1,9 @@
 // import { $ } from "../../utils/snippets";
+import "./_style.scss"
 
 async function StatisticsContent() {
     const MainContent = /*html*/ `
+        <div id="statistics-main-content">
         <div>
             <p>Game Time:</p>
             <p>00:05:03</p>
@@ -14,11 +16,15 @@ async function StatisticsContent() {
             <p>Hit Average:</p>
             <p>...</p>
         </div>
+        </div>
     `
 
     const FooterContent = /*html*/ `
-       <button>Github</button>
-       <button>Linkedin</button>
+        <div id="statistics-footer-content">
+            <button>Github</button>
+            <button>Linkedin</button>
+            <button>Instagram</button>
+       </div>
     `
 
     return [MainContent, FooterContent]
